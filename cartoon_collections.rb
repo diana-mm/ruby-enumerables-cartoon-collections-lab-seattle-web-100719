d04d31b8dc2af2755f4a23edd00bb2d44d7754b5
Diana M <dmmiller85@gmail.com>
2019-10-07 05:19:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,14 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index{ |dwarves, index| puts "#{index +1}. #{dwarf}"}
+  dwarves.each_with_index{ |dwarves, index| puts "#{index +1}. #{dwarves}"}
 end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! {|planeteer_calls| planeteer_calls.capitalize + "!"}
 end
 
-def   planeteer_calls.any? do |call|
+def   long_planteer_calls(planeteer_calls)
+  planeteer_calls.any? do |call|
   call.length > 4
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
