0313090993a41e4b46b15bc335dba4a8d3da51a6
Diana M <dmmiller85@gmail.com>
2019-10-07 05:19:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! {|planeteer_calls| planeteer_calls.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def   planeteer_calls.any? do |call|
+  call.length > 4
 end
 
 def find_the_cheese# code an argument here
